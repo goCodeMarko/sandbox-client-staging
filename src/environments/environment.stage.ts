1e3dc9db7f0626105ecd4a998966be400fb2933e
Patrick Marck Dulaca <patrickmarckdulaca@gmail.com>
2023-05-07 14:15:33 +0800

    first commit

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    SERVER_URL: 'https://www.sandbox-server-staging.com/api/'
+    SERVER_URL: 'https://tender-pink-antelope.cyclic.app/api/'
 };
